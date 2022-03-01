d88653f7c221c76cdf9368c440085a840c1649c7
ElectricBeat <96882916+ElectricBeat@users.noreply.github.com>
2022-03-01 21:12:12 +0530

    LYCASOFT
    LYCASOFT TRINNING CODE FOR ARDUINO UNO REVERCE DIRECTION LED RUNNING CODE

---
+++ REVERSE_LED_BLINKING/REVERSE_LED_BLINKING.ino
@@ -0,0 +1,28 @@
+int LED_1=9;
+int LED_2=10;
+int LED_3=11;
+int LED_4=12;
+
+void setup()
+{
+  pinMode(LED_1,OUTPUT);  
+  pinMode(LED_2,OUTPUT);  
+  pinMode(LED_3,OUTPUT);  
+  pinMode(LED_4,OUTPUT);  
+}
+void loop()
+{
+  digitalWrite(LED_4,HIGH);  
+  delay(1000);  
+  digitalWrite(LED_4,LOW); 
+  digitalWrite(LED_3,HIGH); 
+  delay(1000);  
+  digitalWrite(LED_3,LOW); 
+  digitalWrite(LED_2,HIGH); 
+  delay(1000);  
+  digitalWrite(LED_2,LOW); 
+  digitalWrite(LED_1,HIGH); 
+  delay(1000);  
+  digitalWrite(LED_1,LOW); 
+  delay(1000);  
+}
